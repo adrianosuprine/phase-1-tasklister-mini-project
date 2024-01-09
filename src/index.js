f729b600d56bfc2f99b109303646cd9f65605129
adrianosuprine <adrianosuprine@gmail.com>
2024-01-09 15:51:40 +0300

    learning the DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.getElementById("create-task-form");
+
+  form.addEventListener(("submit"), function (e) {
+      e.preventDefault();
+  
+      let task = document.querySelector('input[name="new-task-description"]').value;
+      let dataToPost = {
+          name:task
+      }
+      // changes the elements in the DOM by adding a new character 
+     let task2 = document.getElementById("tasks");
+     task2.innerHTML = task;
+  
+  })
+  
+
+})
+
